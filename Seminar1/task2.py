f604e8c82ab6a2afe136754a9002159a8ec3c6d4
Петр < >
2023-01-26 21:32:39 +0300

    task03

--- Seminar1/task2.py
+++ Seminar1/task2.py
@@ -3,7 +3,4 @@ class2 = 21
 class3 = 22
 seats = 2 
 
-parts1 = class1 / 2
-parts2 = class2 / 2
-parts3 = class3 / 2
-print((class1+1)//2 +(class2+1)//2 +(class3+1)//2) 
+print((class1+1)//seats +(class2+1)//seats +(class3+1)//seats) 
